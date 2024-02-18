7b8c7277c6ee642ba3ce4c1944a5b0384d9f16dd
IT21284670 <it21284670@my.sliit.lk>
2024-02-18 19:56:00 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,46 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+   
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+    
+    <h1 
+      className='text-3xl font-bold mb-4 text-slate-800'>
+      Luxury Estate: Your Gateway to Extraordinary Living
+    </h1>
+    
+    <p className='mb-4 text-slate-700'>
+      Escape the ordinary and step into the realm of refined living with Luxury Estate. 
+      We're not just a real estate agency, we're your trusted guide to unlocking the door to your 
+      dream property. Nestled in the most coveted enclaves, our curated collection of homes surpasses
+      mere bricks and mortar, offering you havens of unparalleled luxury and bespoke experiences.
+    </p>
+    
+    <p className='mb-4 text-slate-700'>
+    Our passionate team boasts decades of expertise, meticulously navigating the intricacies of the 
+    market to ensure a seamless journey for every client. Whether you envision a sprawling waterfront 
+    estate with breathtaking vistas, a charming pied-à-terre pulsating with city life, or a secluded
+     mountain retreat bathed in tranquility, we possess the key to unlock your desires.
+    
+    </p>
+    
+    <p 
+    className='mb-4 text-slate-700'>
+      At Luxury Estate, we understand that the search for a home is deeply personal. That's why we 
+      cultivate genuine relationships, taking the time to understand your unique aspirations and 
+      lifestyle. Every interaction is infused with an unwavering commitment to personalized service, 
+      exceeding expectations and transforming the process into an enjoyable endeavor.
+    </p>
+
+    <p 
+    className='mb-4 text-slate-700'>
+      So, embark on a journey of exceptional living today. Contact Luxury Estate and allow us to 
+      curate a portfolio of properties that ignite your imagination and exceed your expectations. 
+      Remember, luxury isn't just about price, it's about an elevated experience, and we're here to 
+      guide you every step of the way.
+    </p>
+  
+  </div>
+  
   )
 }
